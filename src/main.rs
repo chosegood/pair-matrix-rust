39c0f45904890871e9f98e6bf35aef65d6d67bea
Chris Hosegood <chris.hosegood@gmail.com>
2024-03-15 20:16:35 +0000

    Remove commented out code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
 use actix_web::{post, web, App, HttpResponse, HttpServer, Responder};
 mod index;
 
-//#[get("/")]
-//async fn hello() -> impl Responder {
-//    HttpResponse::Ok().body("Hello world!")
-//}
-
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
