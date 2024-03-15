a408c676613f8616da7c06aeb9aac3c95d86eef6
Chris Hosegood <chris.hosegood@gmail.com>
2024-03-15 15:06:24 +0000

    add hello world test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", hello());
+}
+
+fn hello() -> String {
+    return "Hello, world".to_string();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = hello();
+        assert_eq!(result, "Hello, world");
+    }
 }
